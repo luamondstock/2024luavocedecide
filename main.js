dc1b00eb4d7877bb9eebc5ce5178191f96c3268a
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-08-09 10:39:02 -0300

    começando java script
    começando java script

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [
+    {
+        enunciado: "texto01",
+        alternativas: [
+            "alternativa01", 
+            "alternativa02"
+        ]
+
+    },
+    {
+        enunciado: "texto02",
+        alternativas: [
+            "alternativa01", 
+            "alternativa02"
+        ]
+
+    },
+
+    
+]
+
 
