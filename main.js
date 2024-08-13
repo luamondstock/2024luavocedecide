4104970040d15ac4ab4f979a3e230fe6edd2390d
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-08-13 11:28:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
 const perguntas = [
     {
@@ -20,8 +21,41 @@ const perguntas = [
         ]
 
     },
+    {
+        enunciado: "texto03",
+        alternativas: [
+            "alternativa01", 
+            "alternativa02"
+        ]
 
+    },
+    {
+        enunciado: "texto04",
+        alternativas: [
+            "alternativa01", 
+            "alternativa02"
+        ]
+
+    },
+    {
+        enunciado: "texto05",
+        alternativas: [
+            "alternativa01", 
+            "alternativa02"
+        ]
+
+    },
     
 ]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = 
+
+}
+
+
 
 
