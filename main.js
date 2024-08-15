50cfed9b6adc2944d0693c1c7f848c57d7d7ef05
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-08-15 08:32:53 -0300

    atualizando

--- main.js
+++ main.js
@@ -8,40 +8,70 @@ const perguntas = [
     {
         enunciado: "texto01",
         alternativas: [
-            "alternativa01", 
-            "alternativa02"
+           { 
+            texto:"alternativa01", 
+            afirmaçao: "afirmaçao01"
+           },
+           { 
+            texto:"alternativa02", 
+            afirmaçao:"afirmaçao02"
+           }
         ]
 
     },
     {
         enunciado: "texto02",
-        alternativas: [
-            "alternativa01", 
-            "alternativa02"
+        alternativas:[
+            { 
+                texto:"alternativa03", 
+                afirmaçao: "afirmaçao03"
+            },
+            {        
+                texto:"alternativa04", 
+                afirmaçao:"afirmaçao04"
+            }
         ]
 
     },
     {
         enunciado: "texto03",
         alternativas: [
-            "alternativa01", 
-            "alternativa02"
+            { 
+                texto:"alternativa05", 
+                afirmaçao: "afirmaçao05"
+               },
+               { 
+                texto:"alternativa06", 
+                afirmaçao:"afirmaçao06"
+            }
         ]
 
     },
     {
         enunciado: "texto04",
         alternativas: [
-            "alternativa01", 
-            "alternativa02"
+            { 
+                texto:"alternativa07", 
+                afirmaçao: "afirmaçao07"
+               },
+               { 
+                texto:"alternativa08", 
+                afirmaçao:"afirmaçao08"
+            }
         ]
 
     },
     {
         enunciado: "texto05",
         alternativas: [
-            "alternativa01", 
-            "alternativa02"
+            { 
+                texto:"alternativa09", 
+                afirmaçao: "afirmaçao09"
+               },
+               { 
+                texto:"alternativa10", 
+                afirmaçao:"afirmaçao10"
+            }
         ]
 
     },
@@ -54,11 +84,21 @@ let perguntaAtual;
 function mostraPergunta(){
     perguntaAtual = perguntas[atual]
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    mostraPergunta();
+    mostraAlternativas();
 }
 
-function mostraAlternativa()
-    perguntaAtual = perguntas[atual]
+function mostraAlternativas()
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostraPergunta();
+        })
+
+        caixaAlternativas.appendChild(botaoAlternativas)
+
+    }
    
 
 
