ef3e286a613a7963b8328907e82fbb63f816f04d
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-08-16 22:47:29 -0300

    uploading

--- main.js
+++ main.js
@@ -1,112 +1,68 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
-const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
 
 const perguntas = [
     {
-        enunciado: "Você é um jovem que teve oportunidade de estudar o idioma de inglês ainda na adolescência. Aos 17 anos, com o certificado de Ingles nas mãos, voce tem que decidir entre: ",
+        enunciado: "texto01",
         alternativas: [
-            {
-                texto:"Continuar morando na sua cidade natal, com pouca possibilidade de deselvonvimento na língua inglesa. ",
-                afirmacao: "Escolha mais segura, seus pais concordam com você e te apoiam nessa decisão. "
-            },
-            {
-                texto:"Fazer um intercâmbio com ajuda remunerada de sua familia para o seu inicio no novo país.",
-                afirmacao: "Maravilha, mesmo ainda sendo novo escolheu se desafiar e se tornar ainda mais fluente."
-            }   
+            "alternativa01", 
+            "alternativa02"
         ]
+
     },
     {
-        enunciado: "Um ano depois você se encontra meio triste com sua escolha. O que fazer agora?",
+        enunciado: "texto02",
         alternativas: [
-            {
-                texto: "Continuar na mesma função que está e não mudar nada.",
-                afirmacao: "Que pena, se arriscar as vezes é bom."
-            },
-            {
-                texto: "alternativa 04",
-                afirmacao: "resultado 04"
-            }
+            "alternativa01", 
+            "alternativa02"
         ]
+
     },
     {
-        enunciado: "Enunciado 03",
+        enunciado: "texto03",
         alternativas: [
-            {
-                texto: "alternativa 05",
-                afirmacao: "resultado 05"
-            },
-            {
-                texto: "alternativa 06",
-                afirmacao: "resultado 06"
-            }
+            "alternativa01", 
+            "alternativa02"
         ]
+
     },
     {
-        enunciado: "Enunciado 04",
+        enunciado: "texto04",
         alternativas: [
-            {
-                texto: "alternativa 07",
-                afirmacao: "resultado 07"
-            },
-            {
-                texto: "alternativa 08",
-                afirmacao: "resultado 08"
-            }
+            "alternativa01", 
+            "alternativa02"
         ]
+
     },
     {
-        enunciado: "Enunciado 05",
+        enunciado: "texto05",
         alternativas: [
-            {
-                texto: "alternativa 09",
-                afirmacao: "resultado 09"
-            },
-            {
-                texto: "alternativa 10",
-                afirmacao: "resultado 10"
-            }
+            "alternativa01", 
+            "alternativa02"
         ]
+
     },
+    
 ]
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal = "";
 
-function mostrarPerguntas(){
-    if(atual >= perguntas.length){
-        mostraResultado();
-        return
-    }
-    perguntaAtual = perguntas[atual];
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual]
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = ""
-    mostrarAlternativas();
+    mostraPergunta();
 }
 
-function mostrarAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
-        caixaAlternativas.appendChild(botaoAlternativas);
-    }
-}
+function mostraAlternativa()
+    perguntaAtual = perguntas[atual]
+   
 
-function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ok";
-    atual++;
-    mostrarPerguntas();
-}
 
-function mostraResultado(){
-    caixaPerguntas.textContent = " Inicio do texto.vsdefrighrgih.. "
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
-}
 
-mostrarPerguntas();
+
+
+
